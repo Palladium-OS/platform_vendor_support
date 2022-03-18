elpaablo/custom-preferences

vendor_support: Pull MainSwitch prefs from https://github.com/AOSP-Krypton/packages_resources_PreferenceExtensions
Change-Id: Ic7ced3722808d5cd4e8643ba225501324e79056e

src/com/spark/support/preferences/GlobalSettingMainSwitchPreference.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.spark.support.preferences
+
+import android.content.Context
+import android.util.AttributeSet
+
+import androidx.core.content.res.TypedArrayUtils
+import androidx.preference.R
+
+import com.android.settingslib.widget.MainSwitchPreference
+
+class GlobalSettingMainSwitchPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = TypedArrayUtils.getAttr(context,
+        R.attr.switchPreferenceStyle,
+        android.R.attr.switchPreferenceStyle),
+    defStyleRes: Int = 0,
+): MainSwitchPreference(context, attrs, defStyleAttr, defStyleRes) {
+    init {
+        setPreferenceDataStore(GlobalSettingsStore(context.contentResolver))
+    }
+}

src/com/spark/support/preferences/SecureSettingMainSwitchPreference.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.spark.support.preferences
+
+import android.content.Context
+import android.util.AttributeSet
+
+import androidx.core.content.res.TypedArrayUtils
+import androidx.preference.R
+
+import com.android.settingslib.widget.MainSwitchPreference
+
+class SecureSettingMainSwitchPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = TypedArrayUtils.getAttr(context,
+        R.attr.switchPreferenceStyle,
+        android.R.attr.switchPreferenceStyle),
+    defStyleRes: Int = 0,
+): MainSwitchPreference(context, attrs, defStyleAttr, defStyleRes) {
+    init {
+        setPreferenceDataStore(SecureSettingsStore(context.contentResolver))
+    }
+}

src/com/spark/support/preferences/SystemSettingMainSwitchPreference.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.spark.support.preferences
+
+import android.content.Context
+import android.util.AttributeSet
+
+import androidx.core.content.res.TypedArrayUtils
+import androidx.preference.R
+
+import com.android.settingslib.widget.MainSwitchPreference
+
+class SystemSettingMainSwitchPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = TypedArrayUtils.getAttr(context,
+        R.attr.preferenceStyle,
+        android.R.attr.preferenceStyle),
+    defStyleRes: Int = 0
+): MainSwitchPreference(context, attrs, defStyleAttr, defStyleRes) {
+    init {
+        setPreferenceDataStore(SystemSettingsStore(context.contentResolver))
+    }
+}

Palladium-OS/platform_vendor_support

src/com/palladium/support/preferences/GlobalSettingMainSwitchPreference.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palladium.support.preferences
+
+import android.content.Context
+import android.util.AttributeSet
+
+import androidx.core.content.res.TypedArrayUtils
+import androidx.preference.R
+
+import com.android.settingslib.widget.MainSwitchPreference
+
+class GlobalSettingMainSwitchPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = TypedArrayUtils.getAttr(context,
+        R.attr.switchPreferenceStyle,
+        android.R.attr.switchPreferenceStyle),
+    defStyleRes: Int = 0,
+): MainSwitchPreference(context, attrs, defStyleAttr, defStyleRes) {
+    init {
+        setPreferenceDataStore(GlobalSettingsStore(context.contentResolver))
+    }
+}

src/com/palladium/support/preferences/SecureSettingMainSwitchPreference.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palladium.support.preferences
+
+import android.content.Context
+import android.util.AttributeSet
+
+import androidx.core.content.res.TypedArrayUtils
+import androidx.preference.R
+
+import com.android.settingslib.widget.MainSwitchPreference
+
+class SecureSettingMainSwitchPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = TypedArrayUtils.getAttr(context,
+        R.attr.switchPreferenceStyle,
+        android.R.attr.switchPreferenceStyle),
+    defStyleRes: Int = 0,
+): MainSwitchPreference(context, attrs, defStyleAttr, defStyleRes) {
+    init {
+        setPreferenceDataStore(SecureSettingsStore(context.contentResolver))
+    }
+}

src/com/palladium/support/preferences/SystemSettingMainSwitchPreference.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palladium.support.preferences
+
+import android.content.Context
+import android.util.AttributeSet
+
+import androidx.core.content.res.TypedArrayUtils
+import androidx.preference.R
+
+import com.android.settingslib.widget.MainSwitchPreference
+
+class SystemSettingMainSwitchPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = TypedArrayUtils.getAttr(context,
+        R.attr.preferenceStyle,
+        android.R.attr.preferenceStyle),
+    defStyleRes: Int = 0
+): MainSwitchPreference(context, attrs, defStyleAttr, defStyleRes) {
+    init {
+        setPreferenceDataStore(SystemSettingsStore(context.contentResolver))
+    }
+}